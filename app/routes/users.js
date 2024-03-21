75602011ec44053db1597914d6b2509ad71e6315
KenilAvaiyaa <avaiyakekenil@gmail.com>
2024-03-21 14:48:35 +0530

    databass for user

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,9 +1,31 @@
-var express = require('express');
-var router = express.Router();
+const mongoose = require('mongoose');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    posts: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post'
+    }],
+    dp: {
+        type: String
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    fullName: {
+        type: String,
+        required: true
+    }
 });
 
-module.exports = router;
+module.exports  = mongoose.model('User', userSchema);
